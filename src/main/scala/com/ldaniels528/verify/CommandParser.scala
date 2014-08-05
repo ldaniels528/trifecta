c4bef30f5c74cf4715a25ba4010ccace7b4bd370
Lawrence Daniels <ldaniels@verisign.com>
2014-08-05 11:11:29 -0400

    added new arguments parsing method

--- src/main/scala/com/ldaniels528/verify/CommandParser.scala
+++ src/main/scala/com/ldaniels528/verify/CommandParser.scala
@@ -5,7 +5,24 @@ package com.ldaniels528.verify
  * @author lawrence.daniels@gmail.com
  */
 object CommandParser {
-  private val SYMBOLS = Set('!', '@', '?')
+  private val SYMBOLS = Set('!', '@', '?', '&')
+
+  /**
+   * Parses the given items (e.g. ["-c", "-f", "myfile"]) into an argument list (e.g. ["-c" -> Nil, "-f" -> "myfile"])
+   * @param items the given array of items
+   * @return the argument list
+   */
+  def parseArgs(items: Seq[String]): List[(String, List[String])] = {
+    val (args, lastArg) = items.map(_.trim).foldRight[(List[(String, List[String])], List[String])]((Nil, Nil)) {
+      case (item, (props, list)) =>
+        if (item.startsWith("-")) ((item -> list) :: props, Nil)
+        else if (item.trim.length > 0) (props, item :: list)
+        else (props, list)
+    }
+
+    // return the argument list
+    if (lastArg.nonEmpty) ("" -> lastArg) :: args else args
+  }
 
   /**
    * Parses the given input string into tokens
