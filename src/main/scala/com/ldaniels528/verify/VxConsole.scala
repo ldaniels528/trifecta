082462add7b129fd925e15b57be0f7f0f8c74602
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-08-24 16:01:14 -0700

    added

---
+++ src/main/scala/com/ldaniels528/verify/VxConsole.scala
@@ -0,0 +1,36 @@
+package com.ldaniels528.verify
+
+import org.fusesource.jansi.Ansi._
+import org.fusesource.jansi.{Ansi, AnsiConsole}
+import org.slf4j.LoggerFactory
+
+/**
+ * Verify Console
+ */
+object VxConsole {
+  private lazy val logger = LoggerFactory.getLogger(getClass)
+  val out = System.out
+
+  def install(): Unit = AnsiConsole.systemInstall()
+
+  def uninstall(): Unit = AnsiConsole.systemUninstall()
+
+  /**
+   * ANSI String Interpolation
+   * @param sc the given string context
+   */
+  implicit class AnsiInterpolation(val sc: StringContext) extends AnyVal {
+
+    def a(args: Any*): Ansi = {
+      val items = sc.parts.iterator
+      val expressions = args.iterator
+
+      val result = (items zip expressions).foldLeft[Ansi](ansi()) { case (results, (s, expr)) =>
+        logger.info(s"a: s = $s, expr = $expr")
+        results
+      }
+      result.reset()
+    }
+  }
+
+}
