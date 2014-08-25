3d531e7a9c777de1f014599fb54e4908f4ff74ef
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-08-24 17:01:14 -0700

    minor updates and variable renaming

--- src/main/scala/com/ldaniels528/verify/VerifyShellRuntime.scala
+++ src/main/scala/com/ldaniels528/verify/VerifyShellRuntime.scala
@@ -1,12 +1,14 @@
 package com.ldaniels528.verify
 
+import java.io.File
 import java.io.File.separator
-import java.io.{ByteArrayOutputStream, File, PrintStream}
 
 import com.ldaniels528.verify.io.EndPoint
 import com.ldaniels528.verify.modules.kafka.KafkaModule
 import com.ldaniels528.verify.modules.zookeeper.{ZKProxy, ZookeeperModule}
 import com.ldaniels528.verify.modules.{CoreModule, ModuleManager}
+import org.fusesource.jansi.Ansi.Color._
+import org.fusesource.jansi.Ansi._
 
 import scala.util.Properties.userHome
 
@@ -15,34 +17,33 @@ import scala.util.Properties.userHome
  * @author lawrence.daniels@gmail.com
  */
 case class VerifyShellRuntime(zkHost: String, zkPort: Int) {
-  val remoteHost = s"$zkHost:$zkPort"
-
-  // redirect standard output and error to my own buffers
+  // capture standard output
   val out = System.out
   val err = System.err
-  val buffer = new ByteArrayOutputStream(16384)
-  System.setOut(new PrintStream(buffer))
 
-  // the default state of the console is "alive"
+  // the Zookeeper remote host
+  val remoteHost = s"$zkHost:$zkPort"
+
+  // get the ZooKeeper end-point
+  val zkEndPoint = EndPoint(zkHost, zkPort)
+
+  // the default state of the application is "alive"
   var alive = true
 
   // various shared state variables
-  var autoSwitching: Boolean = true
+  var autoSwitching = false
   var debugOn = false
   var defaultFetchSize = 1024
   var encoding = "UTF-8"
 
   // ZooKeeper current working directory
-  var zkcwd = "/"
+  var zkCwd = "/"
 
   // the number of columns to display when displaying bytes
   var columns = 25
 
   // define the history properties
   var historyFile = new File(s"$userHome$separator.verify${separator}history.txt")
-
-  // get the ZooKeeper host/port
-  val zkEndPoint = EndPoint(zkHost, zkPort)
 
   // create the ZooKeeper proxy
   val zkProxy = ZKProxy(zkEndPoint)
