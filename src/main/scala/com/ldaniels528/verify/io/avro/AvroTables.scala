c26f127b6f03b031644b21daf74fd998c8b56e34
Lawrence Daniels <ldaniels@verisign.com>
2014-08-27 14:09:23 -0400

    clean up

--- src/main/scala/com/ldaniels528/verify/io/avro/AvroTables.scala
+++ src/main/scala/com/ldaniels528/verify/io/avro/AvroTables.scala
@@ -1,19 +1,21 @@
 package com.ldaniels528.verify.io.avro
 
 import com.ldaniels528.tabular.Tabular
+import org.apache.avro.generic.GenericRecord
+
+import scala.collection.JavaConversions._
 
 /**
  * Adds Avro table capabilities to a Tabular instance
  * @author lawrence.daniels@gmail.com
  */
-trait AvroTables { self : Tabular =>
-  import scala.collection.JavaConversions._
-  import org.apache.avro.generic.GenericRecord
+trait AvroTables {
+  self: Tabular =>
 
   def transformAvro(records: Seq[GenericRecord], reqFields: Seq[String]): Seq[String] = {
     if (records.isEmpty) Seq.empty
     else {
-      val fields = if (reqFields.nonEmpty) reqFields else records.head.getSchema().getFields().map(_.name.trim).toSeq.slice(0, 20)
+      val fields = if (reqFields.nonEmpty) reqFields else records.head.getSchema.getFields.map(_.name.trim).toSeq
       val rows = records map { r =>
         Map(fields map (k => (k, asString(r.get(k)).trim)): _*)
       }
