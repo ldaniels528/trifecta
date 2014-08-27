e8ccfece3c6c5b8d99c3dc53bf88da01dbba0a1f
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-08-26 19:17:58 -0700

    client instance was changed to an Option of a client

--- src/main/scala/com/ldaniels528/verify/modules/storm/StormModule.scala
+++ src/main/scala/com/ldaniels528/verify/modules/storm/StormModule.scala
@@ -1,11 +1,13 @@
 package com.ldaniels528.verify.modules.storm
 
+import backtype.storm.generated.Nimbus
 import backtype.storm.utils.{NimbusClient, Utils}
 import com.ldaniels528.verify.VerifyShellRuntime
 import com.ldaniels528.verify.modules.{Command, Module}
 import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConversions._
+import scala.util.{Failure, Success, Try}
 
 /**
  * Apache Storm Module
@@ -13,16 +15,28 @@ import scala.collection.JavaConversions._
 class StormModule(rt: VerifyShellRuntime) extends Module {
   private val logger = LoggerFactory.getLogger(getClass)
   private val stormConf = Utils.readStormConfig().asInstanceOf[java.util.Map[String, Any]]
-  private val client = NimbusClient.getConfiguredClient(stormConf).getClient
+  private var client: Option[Nimbus.Client] = None
 
-  // the name of the module
+  // load Storm-specific properties
+  rt.configProps foreach { case (k, v) =>
+    if (k.startsWith("storm|")) {
+      stormConf.put(k.drop(6), v)
+    }
+  }
+
+  // connect to the server
+  connect match {
+    case Success(c) => client = Some(c)
+    case Failure(e) =>
+  }
+
   val name = "storm"
 
   override def prompt: String = s"${rt.remoteHost}${rt.zkCwd}"
 
   // the bound commands
   val getCommands = Seq(
-    Command(this, "sconf", showConfig, (Seq.empty, Seq.empty), help = "Lists the Storm configuration"),
+    Command(this, "sconf", showConfig, (Seq.empty, Seq("key", "value")), help = "Lists, retrieves or sets the configuration keys"),
     Command(this, "sdeploy", deployTopology, (Seq("jarfile", "topology"), Seq("arguments")), help = "Deploys a topology to the Storm server"),
     Command(this, "skill", killTopology, (Seq.empty, Seq("topologyName")), help = "Kills a running topology"),
     Command(this, "sls", listTopologies, (Seq.empty, Seq("prefix")), help = "Lists available topologies")
@@ -50,7 +64,7 @@ class StormModule(rt: VerifyShellRuntime) extends Module {
    */
   def killTopology(args: String*): Unit = {
     val topologyName = args.head
-    client.killTopology(topologyName)
+    client.foreach(_.killTopology(topologyName))
   }
 
   /**
@@ -58,9 +72,9 @@ class StormModule(rt: VerifyShellRuntime) extends Module {
    * @example {{ sls }}
    */
   def listTopologies(args: String*): Seq[TopologyDetails] = {
-    client.getClusterInfo.get_topologies_iterator().toSeq map { t =>
+    client.map(_.getClusterInfo.get_topologies_iterator().toSeq map { t =>
       TopologyDetails(t.get_name, t.get_status, t.get_num_workers, t.get_uptime_secs)
-    }
+    }) getOrElse Seq.empty
   }
 
   /**
