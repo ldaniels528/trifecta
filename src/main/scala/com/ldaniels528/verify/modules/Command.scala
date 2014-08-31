e776bf2eb4877918a0f0a107f323269c8d63eb20
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-08-30 22:00:23 -0700

    added prompt aware indicator

--- src/main/scala/com/ldaniels528/verify/modules/Command.scala
+++ src/main/scala/com/ldaniels528/verify/modules/Command.scala
@@ -4,7 +4,12 @@ package com.ldaniels528.verify.modules
  * Represents an Verify Shell command
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
-case class Command(module:Module, name: String, fx: Seq[String] => Any, params: (Seq[String], Seq[String]) = (Seq.empty, Seq.empty), help: String = "") {
+case class Command(module: Module,
+                   name: String,
+                   fx: Seq[String] => Any,
+                   params: (Seq[String], Seq[String]) = (Seq.empty, Seq.empty),
+                   help: String = "",
+                   promptAware: Boolean = false) {
 
   def prototype = {
     val required = (params._1 map (s => s"<$s>")).mkString(" ")
