38e17757bda59505bb34a62831dc576668facaaf
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-09-02 15:44:28 -0700

    refactored compression class and added JUnit tests

--- src/main/scala/com/ldaniels528/verify/io/Compression.scala
+++ src/main/scala/com/ldaniels528/verify/io/Compression.scala
@@ -1,45 +1,48 @@
 package com.ldaniels528.verify.io
 
+import java.io._
+import java.util.zip._
+
 import com.ldaniels528.verify.util.VxUtils._
+import org.apache.commons.io.IOUtils
+
+import scala.util.{Failure, Success, Try}
 
 /**
- * Compression/Decompression Capability
+ * Adds compression/decompression capability to implementing classes
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
 trait Compression {
-  import java.io._
-  import java.util.zip._
-  import scala.util.{ Try, Success, Failure }
-  
+
   /**
    * Compresses the message 
    * @param message the given message
    * @return the compressed copy of the input message
-   * @throws IOException
    */
-  def compress(message: String): Array[Byte] = compress(message.getBytes("UTF-8"))
+  def compress(message: String, encoding: String): Try[Array[Byte]] = compress(message.getBytes(encoding))
 
   /**
    * Compresses the message data
    * @param data the given message data
    * @return the compressed copy of the input data
-   * @throws IOException
    */
-  def compress(data: Array[Byte]): Array[Byte] = {
-    val baos = new ByteArrayOutputStream(data.length)
-    new GZIPOutputStream(baos) { `def`.setLevel(Deflater.BEST_COMPRESSION) } use { gzos =>
+  def compress(data: Array[Byte]): Try[Array[Byte]] = Try {
+    val memStream = new ByteArrayOutputStream(data.length)
+    new GZIPOutputStream(memStream) {
+      `def`.setLevel(Deflater.BEST_COMPRESSION)
+    } use { gzos =>
       gzos.write(data)
       gzos.finish()
-      baos.toByteArray()
+      memStream.toByteArray
     }
   }
-  
+
   /**
    * Attempts to decompress the message however, if the decompression fails
    * the original message will be returned.
    */
   def deflate(message: Array[Byte]): Array[Byte] = {
-    Try(decompress(message)) match {
+    decompress(message) match {
       case Success(decompressed) => decompressed
       case Failure(e) => message
     }
@@ -47,25 +50,44 @@ trait Compression {
 
   /**
    * Decompresses the message data
-   * @param data the given message data
+   * @param compressedData the given message data
    * @return the decompressed copy of the input data
-   * @throws IOException
    */
-  def decompress(compressedData: Array[Byte]): Array[Byte] = {
-    val bais = new ByteArrayInputStream(compressedData)
-    val baos = new ByteArrayOutputStream(3 * compressedData.length)
-    val buf = new Array[Byte](1024)
-    new GZIPInputStream(bais) use { gzin =>
-      var count = 0
-      do {
-        count = gzin.read(buf)
-        if (count > 0) {
-          baos.write(buf, 0, count)
-        }
-      } while (count != -1)
-
-      baos.toByteArray()
+  def decompress(compressedData: Array[Byte]): Try[Array[Byte]] = Try {
+    val out = new ByteArrayOutputStream(compressedData.length * 3)
+    new GZIPInputStream(new ByteArrayInputStream(compressedData)) use { in =>
+      IOUtils.copy(in, out)
+      out.toByteArray
     }
+  }
+
+}
+
+/**
+ * Compression Companion Object
+ * @author Lawrence Daniels <lawrence.daniels@gmail.com>
+ */
+object Compression extends Compression {
+  val self = this
+
+  /**
+   * Syntactic sugar for compressing/decompressing byte arrays
+   */
+  implicit class ByteCompressionHelper(data: Array[Byte]) {
+
+    def compress = self.compress(data)
+
+    def decompress = self.decompress(data)
+
+  }
+
+  /**
+   * Syntactic sugar for compressing/decompressing strings
+   */
+  implicit class StringCompressionHelper(data: String) {
+
+    def compress(encoding: String = "UTF8") = self.compress(data, encoding)
+
   }
 
 }
---
+++ src/test/scala/com/ldaniels528/verify/io/CompressionTest.scala
@@ -0,0 +1,34 @@
+package com.ldaniels528.verify.io
+
+import com.ldaniels528.verify.io.Compression._
+import org.junit._
+import org.slf4j.LoggerFactory
+
+/**
+ * Compression Test Suite
+ * @author Lawrence Daniels <lawrence.daniels@gmail.com>
+ */
+class CompressionTest extends Compression {
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  @Test
+  def testCompression(): Unit = {
+    // define the message
+    val originalMessage = "This is a sample sentence to compress"
+    val encoding = "UTF8"
+
+    // compress & decompress the message
+    val result = for {
+      compressedData <- originalMessage.compress(encoding)
+      uncompressedData <- compressedData.decompress
+    } yield new String(uncompressedData, encoding)
+
+    // compare the results
+    logger.info("The message should have been compressed and decompressed without errors")
+    Assert.assertTrue(result.isSuccess)
+
+    logger.info("The original message should match the result")
+    Assert.assertTrue(result.get == originalMessage)
+  }
+
+}
