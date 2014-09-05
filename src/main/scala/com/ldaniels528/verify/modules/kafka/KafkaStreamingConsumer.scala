ab3a65f3fbc47868069aa40c2f6ee3fc78c6308d
Lawrence Daniels <ldaniels@verisign.com>
2014-09-05 10:31:35 -0400

    added new scan method which allows constraints to be set for matching messages

--- src/main/scala/com/ldaniels528/verify/modules/kafka/KafkaStreamingConsumer.scala
+++ src/main/scala/com/ldaniels528/verify/modules/kafka/KafkaStreamingConsumer.scala
@@ -2,9 +2,10 @@ package com.ldaniels528.verify.modules.kafka
 
 import akka.actor.ActorRef
 import com.ldaniels528.verify.io.EndPoint
-import com.ldaniels528.verify.modules.kafka.KafkaStreamingConsumer.{StreamedMessage, StreamingMessageObserver}
+import com.ldaniels528.verify.modules.kafka.KafkaStreamingConsumer.{Condition, StreamedMessage, StreamingMessageObserver}
 import com.ldaniels528.verify.util.VxUtils._
 import kafka.consumer.{Consumer, ConsumerConfig}
+import kafka.message.MessageAndMetadata
 
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success}
@@ -102,6 +103,43 @@ class KafkaStreamingConsumer(consumerConfig: ConsumerConfig) {
   }
 
   /**
+   * Scans a Kafka topic for the first occurrence of a message matching the given criteria
+   * @param topic the given topic name
+   * @param parallelism the given number of processing threads
+   * @param conditions the given criteria
+   * @return a promise of an option of a streamed message
+   */
+  def scan(topic: String, parallelism: Int, conditions: Seq[Condition])(implicit ec: ExecutionContext): Future[Option[StreamedMessage]] = {
+    val streamMap = consumer.createMessageStreams(Map(topic -> parallelism))
+    var completed: Boolean = false
+    val promise = Promise[Option[StreamedMessage]]()
+
+    // now create an object to consume the messages
+    val tasks = (streamMap.get(topic) map { streams =>
+      (streams map { stream =>
+        Future {
+          val it = stream.iterator()
+          while (!completed && it.hasNext()) {
+            val mam = it.next()
+            if (conditions.forall(_.satisfies(mam))) {
+              promise.success(Option(StreamedMessage(mam.topic, mam.partition, mam.offset, mam.key(), mam.message())))
+              completed = true
+            }
+          }
+        }
+      }).toSeq
+    }).toSeq
+
+    // check for the failure to find the message by key
+    Future.sequence(tasks.flatten).onComplete {
+      case Success(v) => if (!completed) promise.success(None)
+      case Failure(e) => promise.failure(e)
+    }
+
+    promise.future
+  }
+
+  /**
    * Streams data from a Kafka topic to an Akka actor
    * @param topic the given topic name
    * @param parallelism the given number of processing threads
@@ -153,6 +191,10 @@ object KafkaStreamingConsumer {
    * Represents a stream message
    */
   case class StreamedMessage(topic: String, partition: Int, offset: Long, key: Array[Byte], message: Array[Byte])
+
+  trait Condition {
+    def satisfies(mam: MessageAndMetadata[Array[Byte], Array[Byte]]): Boolean
+  }
 
   /**
    * This trait is implemented by classes that are interested in
