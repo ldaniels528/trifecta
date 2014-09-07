7254d27b62e468694ec81b265efed03dcea24bb6
Lawrence Daniels <ldaniels@verisign.com>
2014-09-07 16:55:34 -0400

    updated tests

--- src/test/scala/com/ldaniels528/verify/modules/SimpleParamsSpec.scala
+++ src/test/scala/com/ldaniels528/verify/modules/SimpleParamsSpec.scala
@@ -4,18 +4,18 @@ import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FeatureSpec, GivenWhenThen}
 
 /**
- * Unix-Like Command Parameters Specification
+ * Simple Command Parameters Specification
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
-class UnixLikeParamsSpec() extends FeatureSpec with GivenWhenThen with MockitoSugar {
+class SimpleParamsSpec() extends FeatureSpec with GivenWhenThen with MockitoSugar {
 
-  info("As a Unix-Like Command Parameter instance")
+  info("As a Simple Command Parameter instance")
   info("I want to be able to parse and validate command line input")
 
-  feature("Unix-Like Command Parameter Argument Verification") {
+  feature("Simple Command Parameter Argument Verification") {
     scenario("The Command Parameter instance should parse and verify command line input into arguments") {
       Given("A Command Parameter Set and command line input")
-      val paramSet = UnixLikeParams(required = Seq("key"), flags = Seq("-r" -> "recursive"))
+      val paramSet = SimpleParams(required = Seq("key"), optional = Seq("recursive"))
       val commandLineInput = "zrm -r /some/path/to/delete"
       val command = mock[Command]
 
@@ -26,7 +26,7 @@ class UnixLikeParamsSpec() extends FeatureSpec with GivenWhenThen with MockitoSu
       val params = args.tail
       //intercept[IllegalArgumentException]
       paramSet.checkArgs(command, params)
-      assert(paramSet.transform(params) sameElements List("-r" -> List("/some/path/to/delete")))
+      assert(paramSet.transform(params) sameElements Seq("-r", "/some/path/to/delete"))
     }
   }
 
