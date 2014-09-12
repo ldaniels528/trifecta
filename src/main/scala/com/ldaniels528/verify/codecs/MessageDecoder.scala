8edeb99e2218642548b43b331edff66057df67cd
Lawrence Daniels <ldaniels@verisign.com>
2014-09-12 00:16:27 -0400

    added documentation

--- src/main/scala/com/ldaniels528/verify/codecs/MessageDecoder.scala
+++ src/main/scala/com/ldaniels528/verify/codecs/MessageDecoder.scala
@@ -10,7 +10,15 @@ trait MessageDecoder[T] {
 
   /**
    * Decodes the binary message into a typed object
+   * @param message the given binary message
+   * @return a decoded message wrapped in a Try-monad
    */
   def decode(message: Array[Byte]): Try[T]
+
+  /**
+   * Returns the string representation of the message decoder
+   * @return the string representation of the message decoder
+   */
+  override def toString = getClass.getSimpleName
 
 }
