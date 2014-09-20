f91d97183ba9c5a4936fb82c330a8b95fa1d1ab7
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-09-20 08:50:57 -0700

    added documentation

--- src/main/scala/com/ldaniels528/verify/support/avro/AvroConversion.scala
+++ src/main/scala/com/ldaniels528/verify/support/avro/AvroConversion.scala
@@ -8,6 +8,7 @@ import org.apache.avro.Schema
 import org.apache.avro.generic.{GenericDatumReader, GenericDatumWriter, GenericRecord}
 import org.apache.avro.io.{DecoderFactory, EncoderFactory}
 import org.apache.avro.specific.SpecificRecordBase
+import org.slf4j.LoggerFactory
 
 import scala.util.{Failure, Success, Try}
 
@@ -16,7 +17,13 @@ import scala.util.{Failure, Success, Try}
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
 object AvroConversion {
+  private lazy val logger = LoggerFactory.getLogger(getClass)
 
+  /**
+   * Copies data from a Scala case class to a Java Bean (Avro Builder)
+   * @param bean the given Scala case class
+   * @param builder the given Java Bean (Avro Builder)
+   */
   def copy[A, B](bean: A, builder: B) {
     // retrieve the source data (name-value pairs of the case class)
     val srcData = {
@@ -34,12 +41,24 @@ object AvroConversion {
     }
   }
 
-  def decodeRecord(schema: Schema, data: Array[Byte]): GenericRecord = {
+  /**
+   * Converts the given byte array to an Avro Generic Record
+   * @param schema the given Avro Schema
+   * @param bytes the given byte array
+   * @return an Avro Generic Record
+   */
+  def decodeRecord(schema: Schema, bytes: Array[Byte]): GenericRecord = {
     val reader = new GenericDatumReader[GenericRecord](schema)
-    val decoder = DecoderFactory.get().binaryDecoder(data, null)
+    val decoder = DecoderFactory.get().binaryDecoder(bytes, null)
     reader.read(null, decoder)
   }
 
+  /**
+   * Converts an Avro Java Bean into a byte array
+   * @param schema the given Avro Schema
+   * @param datum the given Avro Java Bean
+   * @return a byte array
+   */
   def encodeRecord[T <: SpecificRecordBase](schema: Schema, datum: T): Array[Byte] = {
     new ByteArrayOutputStream(1024) use { out =>
       val writer = new GenericDatumWriter[GenericRecord](schema)
@@ -59,8 +78,12 @@ object AvroConversion {
       Try(beanClass.getMethod(name, kind)) match {
         case Success(method) => Option(method)
         case Failure(e) =>
-          beanClass.getDeclaredMethods find (m =>
+          val method = beanClass.getDeclaredMethods find (m =>
             m.getName == name && m.getParameterTypes.headOption.exists(isCompatible(_, kind)))
+          if(method.isEmpty) {
+            logger.warn(s"No matching method found for $name with parameter $kind")
+          }
+          method
       }
     }
 
