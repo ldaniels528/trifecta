63e50928b4b0b397f4d13fa125708472c9aa09dc
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-09-20 10:59:13 -0700

    updated the test to include Option[T]

--- src/test/scala/com/ldaniels528/verify/support/avro/AvroConversionSpec.scala
+++ src/test/scala/com/ldaniels528/verify/support/avro/AvroConversionSpec.scala
@@ -1,9 +1,8 @@
 package com.ldaniels528.verify.support.avro
 
 import com.ldaniels528.tabular.Tabular
-import com.ldaniels528.verify.support.avro.AvroConversionSpec.{Student, StudentBuilder}
+import com.ldaniels528.verify.support.avro.AvroConversionSpec.{StudentA, StudentB, StudentBean}
 import org.scalatest.Matchers._
-import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FeatureSpec, GivenWhenThen}
 
 import scala.beans.BeanProperty
@@ -12,26 +11,43 @@ import scala.beans.BeanProperty
  * Avro Conversion Utility Specification
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
-class AvroConversionSpec() extends FeatureSpec with GivenWhenThen with MockitoSugar {
+class AvroConversionSpec() extends FeatureSpec with GivenWhenThen {
   private val tabular = new Tabular()
 
   info("As an Avro Conversion Utility")
-  info("I want to be able to copy data from case classes to Avro Builders")
+  info("I want to be able to copy data from case classes to Java Beans")
 
-  feature("Ability to copy data from a case class to an Avro Builder") {
-    scenario("Compress then decompress a message") {
-      Given("A case class and an Avro builder instance")
-      val student = Student("1502460", "Joseph Smith", 19)
+  feature("Ability to copy data from a Scala case class to a Java Bean") {
+    scenario("Copy values from a Scala case class to a Java Bean instance") {
+      Given("A case class and a Java Bean instance")
+      val student = StudentA("1502460", "Joseph Smith", 19)
       tabular.transform(Seq(student)) foreach (info(_))
-      val builder = new StudentBuilder()
+      val builder = new StudentBean()
 
       When("The copy method is executed")
       AvroConversion.copy(student, builder)
 
-      Then("The builder should contain the values from the case class")
+      Then("The Java Bean should contain the values from the case class")
       student.id shouldBe builder.getId
       student.name shouldBe builder.getName
       student.age shouldBe builder.getAge
+    }
+  }
+
+  feature("Ability to copy data from a case class with Option types to a Java Bean") {
+    scenario("Copy values from a Scala case class with Option types to a Java Bean instance") {
+      Given("A case class and a Java Bean instance")
+      val student = StudentB(Option("1502460"), Option("Joseph Smith"), Option(19))
+      tabular.transform(Seq(student)) foreach (info(_))
+      val builder = new StudentBean()
+
+      When("The copy method is executed")
+      AvroConversion.copy(student, builder)
+
+      Then("The Java Bean should contain the values from the case class")
+      student.id.get shouldBe builder.getId
+      student.name.get shouldBe builder.getName
+      student.age.get shouldBe builder.getAge
     }
   }
 
@@ -39,9 +55,11 @@ class AvroConversionSpec() extends FeatureSpec with GivenWhenThen with MockitoSu
 
 object AvroConversionSpec {
 
-  case class Student(id: String, name: String, age: Int)
+  case class StudentA(id: String, name: String, age: Int)
 
-  class StudentBuilder() {
+  case class StudentB(id: Option[String], name: Option[String], age: Option[Int])
+
+  class StudentBean() {
     @BeanProperty var id: String = _
     @BeanProperty var name: String = _
     @BeanProperty var age: Integer = _
