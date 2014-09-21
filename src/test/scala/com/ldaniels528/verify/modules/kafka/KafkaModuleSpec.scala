5986ba5e9d2a4f1cf79f8d938c5e6ee077d7805c
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-09-20 20:39:06 -0700

    not yet ready for implementation

--- src/test/scala/com/ldaniels528/verify/modules/kafka/KafkaModuleSpec.scala
+++ src/test/scala/com/ldaniels528/verify/modules/kafka/KafkaModuleSpec.scala
@@ -1,63 +1,71 @@
 package com.ldaniels528.verify.modules.kafka
 
+import org.mockito.Mockito.{when => when$}
+import org.scalatest.mock.MockitoSugar
 import org.scalatest.{BeforeAndAfterEach, FeatureSpec, GivenWhenThen}
 
 /**
  * Kafka Module Specification
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
-class KafkaModuleSpec() extends FeatureSpec with BeforeAndAfterEach with GivenWhenThen {
-  //val zkTestServer = new TestingServer(2181)
-  //val zk = ZKProxy("localhost", 2181)
-
-  info("As a Kafka Module")
-  info("I want to be able to execute Kafka Module commands")
-
+class KafkaModuleSpec() extends FeatureSpec with BeforeAndAfterEach with GivenWhenThen with MockitoSugar {
   /*
-  feature("The 'kbrokers' command provides a list of brokers from Zookeeper") {
-    scenario("The command executes without parameters") {
-      Given("A string containing Unix-style parameters with flags")
-      val line = "kget -a schema -f outfile.txt shocktrades.quotes.csv 0 165 -b"
+private val config = new VxConfig()
+private val zk = mock[ZKProxy]
+private val rt = new VxRuntimeContext(config, zk)
 
-      When("The string is parsed into tokens")
-      val tokens = CommandParser.parse(line)
+when$(zk.getChildren("")).thenAnswer(Seq[String](), Nil:_*)
 
-      Then("The tokens are transformed into Unix-style parameters")
-      val result = CommandParser.parseUnixLikeArgs(tokens)
 
-      And("Finally validate the Unix-style parameters")
-      result shouldBe UnixLikeArgs(List("kget", "shocktrades.quotes.csv", "0", "165"), Map("-f" -> Some("outfile.txt"), "-a" -> Some("schema"), "-b" -> None))
-    }
-  }*/
+info("As a Kafka Module")
+info("I want to be able to execute Kafka Module commands")
 
-  feature("Ability to perform a recursive delete of a path in Zookeeper (zrm -r))") {
-    scenario("Recursively delete a path from Zookeeper") {
-      Given("The initial Zookeeper environment")
-      /*
-      if (zk.exists("/consumers").isEmpty) {
-        zk.ensurePath("/consumers/otherTestId")
-        zk.ensurePath("/consumers/myTestId/someSubPath")
-      }
-      if (zk.exists("/brokers").isEmpty) zk.ensurePath("/brokers/ids")
+feature("The 'kbrokers' command provides a list of brokers from Zookeeper") {
+  scenario("The command executes without parameters") {
+    Given("a Kafka Module")
+    val module = new KafkaModule(rt)
 
-      Given("A Verify Run-time Context")
-      val rt = VxRuntimeContext(zk)
+    And("a Zookeeper environment")
+    val ctx = new Object()
+    val data = """{"controller_epoch":3,"leader":1,"version":1,"leader_epoch":0,"isr":[1]}""".getBytes("UTF-8")
+    val paths = (0 to 4).map(n => s"/brokers/topics/test2.Shocktrade.quotes.avro/partitions/$n/state")
 
-      Given("A Zookeeper key/path")
-      val module = new ZookeeperModule(rt)
 
-      When("Executing the recursive delete function")
-      module.delete(UnixLikeArgs(Nil, Map("-r" -> Option("/consumers/myTestId"))))
+    When("the 'brokers' command is executed")
+    val result = module.getBrokers(UnixLikeArgs(Some("brokers"), Nil))
 
-      And("The path (and its children) should no longer exist")
-      val results = rt.zkProxy.getChildren("/consumers")
-      assert(results sameElements Seq("otherTestId"))
-      rt.zkProxy.close()*/
-    }
+    Then("The tokens are transformed into Unix-style parameters")
+    result.isEmpty shouldBe false
   }
+}
+
+
+feature("Ability to perform a recursive delete of a path in Zookeeper (zrm -r))") {
+  scenario("Recursively delete a path from Zookeeper") {
+    Given("The initial Zookeeper environment")
+    if (zk.exists("/consumers").isEmpty) {
+      zk.ensurePath("/consumers/otherTestId")
+      zk.ensurePath("/consumers/myTestId/someSubPath")
+    }
+    if (zk.exists("/brokers").isEmpty) zk.ensurePath("/brokers/ids")
+
+    Given("A Verify Run-time Context")
+    val rt = VxRuntimeContext(zk)
+
+    Given("A Zookeeper key/path")
+    val module = new ZookeeperModule(rt)
+
+    When("Executing the recursive delete function")
+    module.delete(UnixLikeArgs(Nil, Map("-r" -> Option("/consumers/myTestId"))))
+
+    And("The path (and its children) should no longer exist")
+    val results = rt.zkProxy.getChildren("/consumers")
+    assert(results sameElements Seq("otherTestId"))
+    rt.zkProxy.close()
+  }
+}
 
   // all tests are complete, close resources
-  //Try(zk.close())
-  //Try(zkTestServer.close())
+  Try(zk.close())    */
 
 }
