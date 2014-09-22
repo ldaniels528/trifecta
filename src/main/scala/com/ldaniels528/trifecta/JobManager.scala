6979c9f89d7bfb4cd5e5c75d53a4acec531a63aa
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-09-22 16:20:49 -0700

    added

---
+++ src/main/scala/com/ldaniels528/trifecta/JobManager.scala
@@ -0,0 +1,83 @@
+package com.ldaniels528.trifecta
+
+import java.util.Random
+import java.util.concurrent.atomic.AtomicInteger
+
+import com.ldaniels528.trifecta.JobManager._
+
+import scala.collection.mutable
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Success
+
+/**
+ * Job Manager
+ * @author Lawrence Daniels <lawrence.daniels@gmail.com>
+ */
+class JobManager() {
+  private val jobIdGen = new AtomicInteger(new Random().nextInt(1000) + 1000)
+  private val jobs = mutable.Map[Int, JobItem]()
+
+  /**
+   * Creates a new asynchronous job
+   * @param startTime the start time of the job
+   * @param f the asynchronous task
+   * @param command the command that resulted in an asynchronous task
+   * @return  a new job
+   */
+  def createJob(startTime: Long, f: Future[_], command: String)(implicit ec: ExecutionContext): JobItem = {
+    // create the job
+    val job = JobItem(
+      jobId = jobIdGen.incrementAndGet(),
+      startTime = startTime,
+      task = f,
+      command = command)
+
+    // trigger the update for the end time
+    f.onSuccess {
+      case Success(v) => job.endTime = System.currentTimeMillis()
+    }
+
+    // add the job to the mapping
+    jobs += (job.jobId -> job)
+    job
+  }
+
+  /**
+   * Retrieves a job by ID
+   * @param jobId the given job ID
+   * @return the option of a job
+   */
+  def getJobById(jobId: Int): Option[JobItem] = jobs.get(jobId)
+
+  /**
+   * Retrieves a job's task by ID
+   * @param jobId the given job ID
+   * @return the option of a job
+   */
+  def getJobTaskById(jobId: Int): Option[Future[_]] = jobs.get(jobId) map (_.task)
+
+  /**
+   * Retrieves the complete collection of jobs
+   * @return the collection of jobs
+   */
+  def getJobs: Seq[JobItem] = jobs.values.toSeq
+
+  /**
+   * Kills a job by ID
+   * @param jobId the given job ID
+   */
+  def killJobById(jobId: Int): Unit = { jobs -= jobId; () }
+
+}
+
+/**
+ * Job Manager Companion Object
+ * @author Lawrence Daniels <lawrence.daniels@gmail.com>
+ */
+object JobManager {
+
+  case class JobItem(jobId: Int, startTime: Long, task: Future[_], command: String) {
+    var endTime: Long = _
+  }
+
+}
