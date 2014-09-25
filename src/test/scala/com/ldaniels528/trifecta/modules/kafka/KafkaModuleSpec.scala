4032181a0fd11f9c2ead46e7c5d0b21300834b10
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-09-24 17:55:39 -0700

    updated

--- src/test/scala/com/ldaniels528/trifecta/modules/kafka/KafkaModuleSpec.scala
+++ src/test/scala/com/ldaniels528/trifecta/modules/kafka/KafkaModuleSpec.scala
@@ -1,20 +1,25 @@
 package com.ldaniels528.trifecta.modules.kafka
 
+import com.ldaniels528.trifecta.command.UnixLikeArgs
+import com.ldaniels528.trifecta.modules.zookeeper.ZookeeperModule
+import com.ldaniels528.trifecta.support.zookeeper.ZKProxy
+import com.ldaniels528.trifecta.{TxConfig, TxRuntimeContext}
 import org.mockito.Mockito.{when => when$}
+import org.scalatest.Matchers._
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{BeforeAndAfterEach, FeatureSpec, GivenWhenThen}
+
+import scala.util.Try
 
 /**
  * Kafka Module Specification
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
 class KafkaModuleSpec() extends FeatureSpec with BeforeAndAfterEach with GivenWhenThen with MockitoSugar {
-  /*
-private val config = new TxConfig()
-private val zk = mock[ZKProxy]
-private val rt = new TxRuntimeContext(config, zk)
-
-when$(zk.getChildren("")).thenAnswer(Seq[String](), Nil:_*)
+/*
+val config = new TxConfig("host", 2181)
+implicit val zk = mock[ZKProxy]
+implicit val rt = new TxRuntimeContext(config, zk)
 
 
 info("As a Kafka Module")
@@ -23,49 +28,43 @@ info("I want to be able to execute Kafka Module commands")
 feature("The 'kbrokers' command provides a list of brokers from Zookeeper") {
   scenario("The command executes without parameters") {
     Given("a Kafka Module")
-    val module = new KafkaModule(rt)
+    val module = new KafkaModule(config)
 
     And("a Zookeeper environment")
     val ctx = new Object()
     val data = """{"controller_epoch":3,"leader":1,"version":1,"leader_epoch":0,"isr":[1]}""".getBytes("UTF-8")
     val paths = (0 to 4).map(n => s"/brokers/topics/test2.Shocktrade.quotes.avro/partitions/$n/state")
 
-
     When("the 'brokers' command is executed")
-    val result = module.getBrokers(UnixLikeArgs(Some("brokers"), Nil))
+    when$(zk.getChildren("/brokers")).thenReturn(Seq[String]())
+    val result = module.getBrokers(UnixLikeArgs(Option("/brokers"), Nil))
 
     Then("The tokens are transformed into Unix-style parameters")
     result.isEmpty shouldBe false
+    Try(zk.close())
   }
 }
+  feature("Ability to perform a recursive delete of a path in Zookeeper (zrm -r))") {
+    scenario("Recursively delete a path from Zookeeper") {
+      Given("The initial Zookeeper environment")
+      if (zk.exists("/consumers")) {
+        zk.ensurePath("/consumers/otherTestId")
+        zk.ensurePath("/consumers/myTestId/someSubPath")
+      }
+      if (zk.exists("/brokers")) zk.ensurePath("/brokers/ids")
 
+      And("A Zookeeper key/path")
+      val module = new ZookeeperModule(config)
 
-feature("Ability to perform a recursive delete of a path in Zookeeper (zrm -r))") {
-  scenario("Recursively delete a path from Zookeeper") {
-    Given("The initial Zookeeper environment")
-    if (zk.exists("/consumers").isEmpty) {
-      zk.ensurePath("/consumers/otherTestId")
-      zk.ensurePath("/consumers/myTestId/someSubPath")
+      When("Executing the recursive delete function")
+      module.delete(UnixLikeArgs(Some("delete"), Nil, Map("-r" -> Option("/consumers/myTestId"))))
+
+      And("The path (and its children) should no longer exist")
+
+      val results = zk.getChildren("/consumers")
+      results shouldBe Seq("otherTestId")
+      Try(zk.close())
     }
-    if (zk.exists("/brokers").isEmpty) zk.ensurePath("/brokers/ids")
-
-    Given("A Verify Run-time Context")
-    val rt = TxRuntimeContext(zk)
-
-    Given("A Zookeeper key/path")
-    val module = new ZookeeperModule(rt)
-
-    When("Executing the recursive delete function")
-    module.delete(UnixLikeArgs(Nil, Map("-r" -> Option("/consumers/myTestId"))))
-
-    And("The path (and its children) should no longer exist")
-    val results = rt.zkProxy.getChildren("/consumers")
-    assert(results sameElements Seq("otherTestId"))
-    rt.zkProxy.close()
-  }
-}
-
-  // all tests are complete, close resources
-  Try(zk.close())    */
+  } */
 
 }
