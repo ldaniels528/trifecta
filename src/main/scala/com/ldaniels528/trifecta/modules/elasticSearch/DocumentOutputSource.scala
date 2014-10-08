a30cfa3a2a423a97ca3ce970d396e9d09e45c301
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-10-07 21:00:10 -0700

    moved escaping logic into ElasticSearch document output source class

--- src/main/scala/com/ldaniels528/trifecta/modules/elasticSearch/DocumentOutputSource.scala
+++ src/main/scala/com/ldaniels528/trifecta/modules/elasticSearch/DocumentOutputSource.scala
@@ -4,6 +4,7 @@ import com.ldaniels528.trifecta.support.avro.AvroDecoder
 import com.ldaniels528.trifecta.support.elasticsearch.TxElasticSearchClient
 import com.ldaniels528.trifecta.support.io.{KeyAndMessage, OutputSource}
 import com.ldaniels528.trifecta.support.messaging.MessageDecoder
+import org.apache.avro.generic.GenericRecord
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext}
@@ -32,6 +33,7 @@ class DocumentOutputSource(client: TxElasticSearchClient, index: String, objType
               id = escape(id, record),
               doc = record.toString)
             Await.result(task, 60.seconds)
+            ()
           case Failure(e) =>
             throw new IllegalStateException(e.getMessage, e)
         }
@@ -43,5 +45,19 @@ class DocumentOutputSource(client: TxElasticSearchClient, index: String, objType
   }
 
   override def close(): Unit = ()
+
+  /**
+   * Replaces the given identifier with with the value found in the given mapping
+   * @param id the given identifier (e.g. "$symbol")
+   * @param record the given value mapping
+   * @return the mapped value (e.g. "AAPL")
+   */
+  private def escape(id: String, record: GenericRecord): String = {
+    if (!id.startsWith("$")) id
+    else {
+      val key = id.substring(1) // TODO handle recursively? - $$id
+      Option(record.get(key)) map (String.valueOf(_)) map(_.replace(' ', '_')) getOrElse key
+    }
+  }
 
 }
