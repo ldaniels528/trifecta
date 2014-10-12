1070760678a06c9a88a0bd4f687dcddfd3c30d30
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-10-11 21:08:39 -0700

    created a new asynchronous I/O job class

---
+++ src/main/scala/com/ldaniels528/trifecta/AsyncIO.scala
@@ -0,0 +1,23 @@
+package com.ldaniels528.trifecta
+
+import java.util.concurrent.atomic.AtomicLong
+
+import com.google.common.util.concurrent.AtomicDouble
+import com.ldaniels528.trifecta.modules.Module.IOCount
+
+import scala.concurrent.Future
+
+/**
+ * This class acts as a wrapper for an asynchronous job processing
+ * an input, an output both.
+ * @author Lawrence Daniels <lawrence.daniels@gmail.com>
+ */
+case class AsyncIO(startTime: Long, task: Future[_], read: AtomicLong, written: AtomicLong, rps: AtomicDouble) {
+  val endTime = new AtomicLong(0L)
+
+  def getCount: IOCount = {
+    val runTimeSecs = Math.round(10d * ((System.currentTimeMillis() - startTime).toDouble / 1000d)) / 10d
+    IOCount(read.get, written.get, failures = 0, rps.get, runTimeSecs)
+  }
+
+}
