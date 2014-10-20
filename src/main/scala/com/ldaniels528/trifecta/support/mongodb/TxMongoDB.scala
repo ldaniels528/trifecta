5ad10c46fc6d708c1b17de22ce92a35b5791b000
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-10-19 18:53:38 -0700

    added method to close the database connection

--- src/main/scala/com/ldaniels528/trifecta/support/mongodb/TxMongoDB.scala
+++ src/main/scala/com/ldaniels528/trifecta/support/mongodb/TxMongoDB.scala
@@ -2,11 +2,21 @@ package com.ldaniels528.trifecta.support.mongodb
 
 import com.mongodb.casbah.Imports._
 
+import scala.util.Try
+
 /**
  * Trifecta MongoDB Database Connection
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
 case class TxMongoDB(databaseName: String, conn: MongoConnection) {
+
+  /**
+   * Closes the database connection
+   */
+  def close() {
+    Try(conn.close())
+    ()
+  }
 
   /**
    * Returns a new reference to the specified collection
