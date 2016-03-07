2f02786881454c41f8435faf230eff381f66cd55
Lawrence Daniels <lawrence.daniels@gmail.com>
2016-03-06 20:53:13 -0800

    re-added Trifecta CLI web dependencies

---
+++ app-cli/src/main/resources/app/js/services/ZookeeperSvc.js
@@ -0,0 +1,34 @@
+/**
+ * Zookeeper Service
+ * @author Lawrence Daniels <lawrence.daniels@gmail.com>
+ */
+(function () {
+    angular.module('trifecta')
+        .factory('ZookeeperSvc', function ($http) {
+            var service = {};
+
+            service.getZkData = function (path, format) {
+                return $http.get("/rest/getZkData" + path + "/" + format)
+                    .then(function (response) {
+                        return response.data;
+                    });
+            };
+
+            service.getZkInfo = function (path) {
+                return $http.get("/rest/getZkInfo" + path)
+                    .then(function (response) {
+                        return response.data;
+                    });
+            };
+
+            service.getZkPath = function (path) {
+                return $http.get("/rest/getZkPath" + path)
+                    .then(function (response) {
+                        return response.data;
+                    });
+            };
+
+            return service;
+        });
+
+})();
