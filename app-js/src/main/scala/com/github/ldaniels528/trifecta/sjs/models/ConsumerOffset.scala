3585d857b8fcc3d054259f6c1708c9042b8c1a16
Lawrence Daniels <lawrence.daniels@gmail.com>
2017-01-18 15:26:49 -0800

    1. Fixed issues with consumer offset updates 2. Removed obsolete REST calls 3. Fixed issue with handling Futures in Scripting mode

--- app-js/src/main/scala/com/github/ldaniels528/trifecta/sjs/models/ConsumerOffset.scala
+++ app-js/src/main/scala/com/github/ldaniels528/trifecta/sjs/models/ConsumerOffset.scala
@@ -8,8 +8,15 @@ import scala.scalajs.js.annotation.ScalaJSDefined
   * @author lawrence.daniels@gmail.com
   */
 @ScalaJSDefined
-class ConsumerOffset(var consumerId: js.UndefOr[String] = js.undefined,
-                     var topic: js.UndefOr[String] = js.undefined,
+class ConsumerOffset(var topic: js.UndefOr[String] = js.undefined,
                      var partition: js.UndefOr[Int] = js.undefined,
-                     var offset: js.UndefOr[Int] = js.undefined,
-                     var lastModifiedTime: js.UndefOr[Double] = js.undefined) extends js.Object
+                     var offset: js.UndefOr[Double] = js.undefined,
+                     var topicStartOffset: js.UndefOr[Double] = js.undefined,
+                     var topicEndOffset: js.UndefOr[Double] = js.undefined,
+                     var messages: js.UndefOr[Double] = js.undefined,
+                     var lastModifiedTime: js.UndefOr[Double] = js.undefined) extends js.Object {
+
+  var expanded: js.UndefOr[Boolean] = js.undefined
+  var deltaC: js.UndefOr[Double] = js.undefined
+  var deltaT: js.UndefOr[Double] = js.undefined
+}
